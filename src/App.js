26e7e6fe8185991253938c76a62bf15e27f0412a
unknown <rxeeb@yandex.ru>
2021-08-09 02:18:12 +0300

    react router add

--- src/App.js
+++ src/App.js
@@ -1,70 +1,74 @@
 import axios from 'axios';
 import React from 'react'
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 import ColorPicker from './components/ColorPicker';
 import Palette from './components/Palette';
 
 
 function App() {
-  const [items , setItems] = React.useState([])
-  const [paletteItems,setPaletteItems] = React.useState([])
+  const [items, setItems] = React.useState([])
+  const [paletteItems, setPaletteItems] = React.useState([])
   const [currentColor, setCurrentColor] = React.useState('#FF453A')
   const [isAdded, setIsAdded] = React.useState(null)
   const [isDeleted, setIsDeleted] = React.useState(null)
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     axios.get('https://610a1f3352d56400176afc0f.mockapi.io/colors').then((res) => {
       setItems(res.data)
     })
     axios.get('https://610a1f3352d56400176afc0f.mockapi.io/batapalette').then((res) => {
       setPaletteItems(res.data)
     })
-    
-  },[])
 
-const onAddToPalette = (obj) => {
-  axios.post('https://610a1f3352d56400176afc0f.mockapi.io/batapalette',obj)
-  setPaletteItems((prev)  => [...prev , obj])
+  }, [])
+
+  const onAddToPalette = (obj) => {
+    axios.post('https://610a1f3352d56400176afc0f.mockapi.io/batapalette', obj)
+    setPaletteItems((prev) => [...prev, obj])
   }
- const onRemoveItem = (id) => {
-   console.log(id)
-  axios.delete(`https://610a1f3352d56400176afc0f.mockapi.io/batapalette/${id}`)
-  setPaletteItems((prev)  => prev.filter(item => item.id !== id))
- }
+  const onRemoveItem = (id) => {
+    console.log(id)
+    axios.delete(`https://610a1f3352d56400176afc0f.mockapi.io/batapalette/${id}`)
+    setPaletteItems((prev) => prev.filter(item => item.id !== id))
+  }
 
   return (
     <div className="App">
+      <Link to="/">формы</Link>
+      <Link to="/palette">палитра</Link>
+      
 
-      <div className='wrapper'>
-        <div className='palette_container'>
-        <Palette 
-        currentColor={currentColor}
-        setCurrentColor ={setCurrentColor}
-        addedColors ={paletteItems}
-        onRemoveItem ={onRemoveItem}
-        />
+        <div className='wrapper'>
+        <Route path="/palette" exact>
+          <div className='palette_container'>
+            <Palette
+              currentColor={currentColor}
+              setCurrentColor={setCurrentColor}
+              addedColors={paletteItems}
+              onRemoveItem={onRemoveItem}
+            />
+          </div>
+          <hr></hr>
+          <div className='colorPicker_container'>
+            {
+              items.map((item, id) => (
+                <ColorPicker
+                  key={id}
+                  {...item}
+                  onColor={(obj) => onAddToPalette(obj)}
+                  onDeleteColor={() => console.log('удалено')}
+                />
+              ))
+            }
+          </div>
+
+          <button className='btn'>Добавить цвет</button>
+          </Route>
         </div>
-        <hr></hr>
-        <div className='colorPicker_container'>
-          {
-            items.map((item, id) => (
-              <ColorPicker
-                key ={id}
-                {...item}
-                onColor = {(obj) => onAddToPalette(obj)}
-                onDeleteColor={() => console.log('удалено')}
-              />
-            ))
-          }
-        </div>
+     
+      
 
-        <button className='btn'>Добавить цвет</button>
-
-      </div>
-      {/* <ColorPicker 
-
-    color ={colors}
-    onClickColor ={() =>console.log('plus')}/> */}
     </div>
   );
 }
